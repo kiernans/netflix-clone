4369af955b57d5e85598d6defb6a9298739b4b76
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-09-13 16:44:08 -0700

    Add link to Browse route

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from "react";
+import { Link } from "react-router-dom";
 
 const Home: FC = () => {
-  return <div>HOME</div>;
+  return (
+    <Link to="browse" style={{ color: "white" }}>
+      BROWSE
+    </Link>
+  );
 };
 
 export default Home;
